60e1681b80da42a777c73bc5c0ae084daca8d30d
Harshil Shah <shah.harshil2@northeastern.edu>
2022-11-22 01:46:56 -0500

    adding basic redux toolkit configuration

--- ieltstar/pages/_app.tsx
+++ ieltstar/pages/_app.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../material-ui-configs/theme';
 import createEmotionCache from '../material-ui-configs/createEmotionCache';
+import { wrapper } from '../store';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -14,7 +15,7 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-export default function MyApp(props: MyAppProps) {
+const myApp = (props: MyAppProps) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
@@ -29,3 +30,5 @@ export default function MyApp(props: MyAppProps) {
     </CacheProvider>
   );
 }
+
+export default wrapper.withRedux(myApp);
