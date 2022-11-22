ccb0cd1e585f70cb6a83f9ec9a5d281f599a42ab
Harshil Shah <shah.harshil2@northeastern.edu>
2022-11-22 11:58:55 -0500

    redux sample reducer

--- ieltstar/pages/index.tsx
+++ ieltstar/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.scss'
 import Head from 'next/head'
+import {Counter} from '../components/testStore'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        
+        <Counter />
       </main>
     </>
   )
