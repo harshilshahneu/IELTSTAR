620e61e6e71d0da332c8a437ddcdecdb98c92ae2
Harshil Shah <shah.harshil2@northeastern.edu>
2022-11-28 02:03:51 -0500

    setting up user slice in redux

--- ieltstar/pages/index.tsx
+++ ieltstar/pages/index.tsx
@@ -1,6 +1,15 @@
 import styles from '../styles/Home.module.scss';
+import { useSelector } from 'react-redux';
+
+interface User {
+  user: {
+    user: object
+  };
+}
 
 export default function Home() {
+  const user = useSelector((state: User) => state.user.user);
+  console.log(user);
   return (
     <>
      
