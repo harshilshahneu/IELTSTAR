ca2f8d863ac136b2fed4d765cca34d46f66428f4
Harshil Shah <shah.harshil2@northeastern.edu>
2022-11-29 16:07:19 -0500

    data table for questions

--- ieltstar/pages/admin/reading.jsx
+++ ieltstar/pages/admin/reading.jsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react'
 import Admin from '../../components/Layout/Admin'
+import axios from 'axios'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const reading = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:8080/questions')
+        .then(response => setData(response.data))
+  }, [])
   return (
-    <div>
-      
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="questions crud table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Question Title</TableCell>
+            <TableCell align="right">Question Description</TableCell>
+            <TableCell align="right">Question Type</TableCell>
+            <TableCell align="right">Options</TableCell>
+            <TableCell align="right">Answer</TableCell>
+            <TableCell align="right">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.questionTitle}
+              </TableCell>
+              <TableCell align="right">{row.questionDescription}</TableCell>
+              <TableCell align="right">{row.questionType}</TableCell>
+              <TableCell align="right">{row.questionOptions}</TableCell>
+              <TableCell align="right">{row.correctAnswer}</TableCell>
+              <TableCell align="right">
+
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
