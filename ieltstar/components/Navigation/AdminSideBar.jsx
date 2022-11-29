a065aed91b7ba2dae5a31944827e7829a5c2b2a1
Harshil Shah <shah.harshil2@northeastern.edu>
2022-11-29 15:40:34 -0500

    admin panel layout

---
+++ ieltstar/components/Navigation/AdminSideBar.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import Link from "next/link";
+import { tokens } from "../../material-ui-configs/theme";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+      routerLink={<Link href={to} />}
+    >
+      <Typography>{title}</Typography>
+    </MenuItem>
+  );
+};
+
+const AdminSideBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .menu-anchor:hover": {
+          background: `${theme.palette.background.default}`,
+        },
+        "& .menu-item.active>a": {
+          background: `${theme.palette.background.default}`,
+        },
+      }}
+    >
+      <Sidebar collapsed={isCollapsed} style={{height: '100%'}}>
+        <Menu>
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+
+          <Box>
+            <Item
+              title="Reading"
+              to="./reading"
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Listening"
+              to="./listening"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Writing"
+              to="./writing"
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            
+          </Box>
+        </Menu>
+      </Sidebar>
+    </Box>
+  );
+};
+
+export default AdminSideBar;
