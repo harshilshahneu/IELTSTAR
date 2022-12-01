204ca27d26cc38ba58962917bc73788994821c78
Harshil Shah <shah.harshil2@northeastern.edu>
2022-12-01 16:35:28 -0500

    test api with question cleanup

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,13 +1,12 @@
 //Pass the app to the routes
-
-import questionRouter from "./question-router.js";
 import scoreRouter from "./score-router.js";
 import examRouter from "./exam-router.js";
+import testRouter from "./test-router.js";
 
 const routes = (app) => {
-  app.use("/questions", questionRouter);
   app.use("/score", scoreRouter);
   app.use("/exams", examRouter);
+  app.use("/tests", testRouter);
 };
 
 export default routes;
---
+++ server/api/routes/test-router.js
@@ -0,0 +1,30 @@
+import express from "express";
+import * as testController from "../controllers/test-controller.js";
+
+const Router = express.Router();
+
+//Create a test for a particular exam
+Router.route('/')
+    .post(testController.createTest)
+
+//Get, Put and Delete routes with test id as the wildcard
+Router.route('/:id')
+    .get(testController.getTestById)
+    .put(testController.updateTest)
+    .delete(testController.deleteTest);
+
+//Add a question to a test
+Router.route('/:id/questions') 
+    .post(testController.addQuestionToTest)
+
+//Update or delete a question in the test
+Router.route('/:id/questions/:questionId')
+    .put(testController.updateQuestionInTest)
+    .delete(testController.deleteQuestionFromTest)    
+
+//Get a test of particular type by exam id
+Router.route('/:examId/:category')
+    .get(testController.getTestByExamIdAndCategory)
+   
+
+export default Router;
