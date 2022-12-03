1e0d433b0cd6dafa44b2fdecdaa2f634f03e68a6
keerthana <lnu.ke@northeastern.edu>
2022-12-03 18:48:46 -0500

    landing pagemodified

--- ieltstar/pages/index.tsx
+++ ieltstar/pages/index.tsx
@@ -1,39 +1,72 @@
-import styles from "../styles/Home.module.scss";
-import CssBaseline from "@mui/material/CssBaseline";
-import Hero from "./../components/Hero";
-import Header from "./../components/Header";
-import Section from "./../components/Section";
-import Testimonial from "./../components/Testimonial";
-import ContactUs from "./../components/ContactUs";
-import Footer from "./../components/Footer";
-import AboutUs from "./../components/AboutUs.js";
+import Layout from "components/layout/Layout";
 
-import { useSelector } from "react-redux";
+import { Container, Grid, Typography, Avatar, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-interface User {
-  user: {
-    user: object;
-  };
-}
+const useStyles = makeStyles((theme) => ({
+  btn: {
+    background: theme.palette.primary.main,
+    color: theme.palette.secondary.main,
+    border: `1px solid ${theme.palette.primary.main}`,
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
+  },
+  img: {
+    width: "100%",
+    height: "auto",
+    boxShadow: "0px 2px 20px rgba(0,0,0,0.4)",
+  },
+}));
 
-const Home = () => {
-  const user = useSelector((state: User) => state.user.user);
+const About = () => {
+  const classes = useStyles();
   return (
-    <>
-      <div>
-        <CssBaseline />
-        <Header />
-        <Hero />
-        <Section />
-        <AboutUs />
-        <Testimonial />
-        <ContactUs />
-        <Footer />
-      </div>
-    </>
+    <Layout
+      // type your page title and page description.
+      title="Template - Next.js and Material-UI with Header and Footer"
+      description="This is a Template using Next.js and Material-UI with Header and Footer."
+    >
+      <Container maxWidth="md">
+        <Typography
+          variant="h1"
+          align="center"
+          gutterBottom
+          style={{ marginBottom: "1em" }}
+        >
+          Template - Next.js and Material-UI with Header and Footer
+        </Typography>
+        <Grid container direction="column" alignItems="center" spacing={4}>
+          <Grid item>
+            <Button
+              component={"a"}
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://github.com/SatoruAkiyama/nextjs-and-material-ui-template-with-header-and-footer/"
+              className={classes.btn}
+            >
+              Get Started
+            </Button>
+          </Grid>
+          <Grid item>
+            <Container maxWidth="sm">
+              <Typography variant="h2" align="center">
+                This is a Template using Next.js and Material-UI with Header and
+                Footer.
+              </Typography>
+            </Container>
+          </Grid>
+          <Grid item>
+            <img
+              src="https://i.imgur.com/1H2TK2B.png"
+              alt="img"
+              className={classes.img}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
   );
 };
-Home.getLayout = function getLayout(page: any) {
-  return <>{page}</>;
-};
-export default Home;
+
+export default About;
