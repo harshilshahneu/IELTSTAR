6dc4ac9603fc852ff48ee8eec0df5e24b5ae783a
keerthana <lnu.ke@northeastern.edu>
2022-12-04 15:23:08 -0500

    landing page

--- ieltstar/pages/index.tsx
+++ ieltstar/pages/index.tsx
@@ -1,17 +1,39 @@
-import styles from '../styles/Home.module.scss';
-import { useSelector } from 'react-redux';
-
+import styles from "../styles/Landing.module.scss";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useSelector } from "react-redux";
+import Hero from "../components/LandingPage/Hero";
+import Header from "../components/LandingPage/Header";
+import Section from "../components/LandingPage/Section";
+import AboutUs from "../components/LandingPage/AboutUs";
+import Testimonial from "../components/LandingPage/Testimonial";
+import Footer from "../components/LandingPage/Footer";
+import ContactUs from "../components/LandingPage/ContactUs";
+import Image from "next/image";
+import backgroungImg from "../images/backgroundImg.jpg";
 interface User {
   user: {
-    user: object
+    user: object;
   };
 }
 
-export default function Home() {
+const Home = () => {
   const user = useSelector((state: User) => state.user.user);
   return (
     <>
-     
+      <div className={styles.bgWrap}>
+        <CssBaseline />
+        <Header />
+        <Hero />
+        <Section />
+        <AboutUs />
+        <Testimonial />
+        <ContactUs />
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
+Home.getLayout = function getLayout(page: any) {
+  return <>{page}</>;
+};
+export default Home;
