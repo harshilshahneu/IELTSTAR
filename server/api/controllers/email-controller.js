da724e890d72649aeaf1d23f6d6241cc05cd1c1b
keerthana <lnu.ke@northeastern.edu>
2022-12-06 17:37:39 -0500

    Adding all variables for dynamic template of sendgrid

--- server/api/controllers/email-controller.js
+++ server/api/controllers/email-controller.js
@@ -19,13 +19,6 @@ const setResponse = (res, status, data) => {
 // get all exams
 export const sendEmails = async (req, res) => {
   const id = JSON.stringify(req.params.id);
-  console.log(req.body);
-  console.log(req.body.name);
-  console.log(req.body.scores.readingScore);
-  console.log(req.body.scores.writingScore);
-  console.log(req.body.scores.speakingScore);
-  console.log(req.body.scores.listeningScore);
-  console.log(req.body.scores.overallBand);
   const options = {
     from: `${process.env.FROM_EMAIL}`,
     to: id,
@@ -33,26 +26,23 @@ export const sendEmails = async (req, res) => {
     subject: "Ielts Test Scores",
     templateId: "d-b2de4eb090e841de8e0a207a02261464",
     dynamic_template_data: {
-      email: "techgeniusk@gmail.com",
-      name: "Keerthana",
-      picture:
-        "https://lh3.googleusercontent.com/a/AEdFTp4sRTB5MXNOZ1jhtxIPDL_khsKwdPWXDdGQ-CqOJxA=s96-c",
+      email: req.body.email,
+      name: req.body.name,
+      picture: req.body.picture,
       scores: {
         _id: "638663ce29b99e23f2099cdf",
         testId: "2",
-        readingScore: 7,
-        writingScore: 9,
-        speakingScore: 9,
-        listeningScore: 9,
-        overallBand: 7,
-        testDate: "2022-12-01T03:49:56.662Z",
-        id: "638663ce29b99e23f2099cdf",
+        readingScore: req.body.scores.readingScore,
+        writingScore: req.body.scores.writingScore,
+        speakingScore: req.body.scores.speakingScore,
+        listeningScore: req.body.scores.listeningScore,
+        overallBand: req.body.scores.overallBand,
       },
     },
   };
   try {
     //   send email
-    // await mailer.sendMail(options);
+    await mailer.sendMail(options);
     res.status(200).json({
       message: "success",
     });
