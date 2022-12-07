58a562033c621a4dc4e2d55b935cd8b3e26ffa8a
Harshil Shah <shah.harshil2@northeastern.edu>
2022-12-07 13:35:52 -0500

    dynamic summary

--- ieltstar/pages/student/dashboard.jsx
+++ ieltstar/pages/student/dashboard.jsx
@@ -1,5 +1,5 @@
 import { Typography, Container, Grid } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Iconify from "../../components/Iconify";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -8,25 +8,76 @@ import SectionWiseComparisonChart from "../../components/Student/Dashboard/Secti
 import Leaderboard from "../../components/Student/Dashboard/Leaderboard";
 import TestTimeline from "../../components/Student/Dashboard/TestTimeline";
 import SuggestedStudyMaterial from "../../components/Student/Dashboard/SuggestedStudyMaterial";
+import BoltIcon from '@mui/icons-material/Bolt';
+import Box from "@mui/material/Box";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import axios from "axios";
 
 const dashboard = () => {
+  const user = useUser().user;
+  const [userData, setUserData] = useState({});
+  const [summary, setSummary] = useState({
+    reading: 0,
+    listening: 0,
+    writing: 0,
+    speaking: 0,
+  });
+
   useEffect(() => {
     AOS.init({
       once: true,
     });
   }, []);
 
+  useEffect(() => {
+    if (user) {
+      axios.get(`${process.env.API_URL}/students/email/${user.email}`)
+        .then(user => {
+          setUserData(user.data);
+          user.data.testHistory.forEach(test => {
+            console.log(test.score)
+            if(test.testType === "Reading")
+              setSummary(prev => ({...prev, reading: prev.reading + test.score}))
+            if(test.testType === "Listening")
+              setSummary(prev => ({...prev, listening: prev.listening + test.score}))
+            if(test.testType === "Writing")
+              setSummary(prev => ({...prev, writing: prev.writing + test.score}))
+            if(test.testType === "Speaking")
+              setSummary(prev => ({...prev, speaking: prev.speaking + test.score}))
+          })
+          //average the sores
+          setSummary(prev => {
+            const readingTests = user.data.testHistory.filter(test => test.testType === "Reading").length ? user.data.testHistory.filter(test => test.testType === "Reading").length : 1
+            const listeningTests = user.data.testHistory.filter(test => test.testType === "Listening").length ? user.data.testHistory.filter(test => test.testType === "Listening").length : 1
+            const writingTests = user.data.testHistory.filter(test => test.testType === "Writing").length ? user.data.testHistory.filter(test => test.testType === "Writing").length : 1
+            const speakingTests = user.data.testHistory.filter(test => test.testType === "Speaking").length ? user.data.testHistory.filter(test => test.testType === "Speaking").length : 1
+
+            return {
+              reading: prev.reading / readingTests,
+              listening: prev.listening / listeningTests,
+              writing: prev.writing / writingTests,
+              speaking: prev.speaking / speakingTests,
+            }
+          })
+
+        })
+    }
+  }, [user]);
+
   return (
     <Container maxWidth="xl">
-      <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi, Welcome back
-      </Typography>
+      <Box sx={{ display: "flex", alignItems: "center",  mb: 5  }}>
+        <Typography variant="h4" sx={{}}>
+          Hi, Welcome
+        </Typography>
+        <BoltIcon fontSize="large" color="warning" />
+      </Box>
 
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6} md={3} data-aos="fade-up">
           <AppWidgetSummary
             title="Reading"
-            total={55}
+            total={summary.reading}
             color="success"
             icon={"fluent-mdl2:reading-mode"}
           />
@@ -42,7 +93,7 @@ const dashboard = () => {
         >
           <AppWidgetSummary
             title="Listening"
-            total={44}
+            total={summary.listening}
             color="info"
             icon={"grommet-icons:assist-listening"}
           />
@@ -58,7 +109,7 @@ const dashboard = () => {
         >
           <AppWidgetSummary
             title="Writing"
-            total={33}
+            total={summary.writing}
             color="warning"
             icon={"icon-park-outline:writing-fluently"}
           />
@@ -74,7 +125,7 @@ const dashboard = () => {
         >
           <AppWidgetSummary
             title="Speaking"
-            total={77}
+            total={summary.speaking}
             color="error"
             icon={"iconoir:mic-speaking"}
           />
