681908191f970c7027438ad4914ad8afcfdc5be7
Harshil Shah <shah.harshil2@northeastern.edu>
2022-12-07 17:51:44 -0500

    handling null values for writing

--- server/api/controllers/student-controller.js
+++ server/api/controllers/student-controller.js
@@ -96,6 +96,19 @@ export const addTestHistory = async (req, res) => {
         }
         else if(testType === "Writing") {
             //handle score for writing
+            console.log(req.body.score)
+            req.body.score = req.body.score.map(score => {
+                if(score === 0) {
+                    console.log(score)
+                    return {
+                        suggestionsCount : 0
+                    }
+                }
+                else {
+                    return score
+                }
+            })
+            console.log(req.body.score)
             req.body.score = req.body.score[0].suggestionsCount + req.body.score[1].suggestionsCount
         }
         else if(testType === "Speaking") {
