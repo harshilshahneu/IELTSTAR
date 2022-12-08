eb778cfb5002e05fb568e7ab937b4bef9815e676
Harshil Shah <shah.harshil2@northeastern.edu>
2022-12-07 20:07:03 -0500

    admin route security

--- ieltstar/pages/index.tsx
+++ ieltstar/pages/index.tsx
@@ -2,8 +2,8 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import axios from "axios";
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 interface User {
   user: {
@@ -19,30 +19,36 @@ const Home = () => {
 
   useEffect(() => {
     if (user) {
-      //handle log-in or sign-up
-      axios
-        .get(`${process.env.API_URL}/students/email/${user.email}`)
-        .then((res) => {
-          console.log(res);
-          if (res.data === null) {
-            //create new student
-            axios
-              .post(`${process.env.API_URL}/students`, {
-                email: user.email,
-                name: user.name,
-                profileURL: user.picture,
-              })
-              .then((res) => {
-                console.log(res);
-                router.push("/student/dashboard");
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          } else {
-            router.push("/student/dashboard");
-          }
-        });
+      //redirect to admin if admin
+      console.log(user.email);
+      if (user.email === "admin@gmail.com") {
+        router.push("/admin/exam");
+      } else {
+        //handle log-in or sign-up
+        axios
+          .get(`${process.env.API_URL}/students/email/${user.email}`)
+          .then((res) => {
+            console.log(res);
+            if (res.data === null) {
+              //create new student
+              axios
+                .post(`${process.env.API_URL}/students`, {
+                  email: user.email,
+                  name: user.name,
+                  profileURL: user.picture,
+                })
+                .then((res) => {
+                  console.log(res);
+                  router.push("/student/dashboard");
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            } else {
+              router.push("/student/dashboard");
+            }
+          });
+      }
     } else {
       //when user logs-out
       router.push("/landing");
