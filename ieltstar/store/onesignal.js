db429b05a35f3a300cd30bb99fdb138ce2a78098
keerthana <lnu.ke@northeastern.edu>
2022-12-07 21:34:18 -0500

    adding to env folder

--- ieltstar/store/onesignal.js
+++ ieltstar/store/onesignal.js
@@ -2,7 +2,7 @@ import OneSignal from "react-onesignal";
 
 export default async function runOneSignal() {
   await OneSignal.init({
-    appId: "9ef61c14-24cb-44a0-a382-95a6a29bb038",
+    appId: process.env.ONE_SIGNAL,
     allowLocalhostAsSecureOrigin: true,
   });
   OneSignal.showSlidedownPrompt();
