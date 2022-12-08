5ea9a0dbb5bc018d2701a2c1ea5ef5a117327ca2
Amey Bansod <bansod.a@northeastern.edu>
2022-12-07 21:44:20 -0500

    Share buttons

--- ieltstar/components/ScoreBoard/ScoreBoard.jsx
+++ ieltstar/components/ScoreBoard/ScoreBoard.jsx
@@ -1,3 +1,5 @@
+import {InlineReactionButtons} from 'sharethis-reactjs';
+import {InlineShareButtons} from 'sharethis-reactjs';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
@@ -98,8 +100,56 @@ export default function ScoreBoard({open, setOpen}) {
                         >
                             <CloseIcon />
                         </IconButton>
-                        <div className={styles.profileTitle}>{user ? user.nickname : 'GUEST'}</div>
-
+                        
+                        <div className={styles.profileTitle}>Can you show your support {user ? user.nickname : 'GUEST'} by sharing our website with your friends?</div>
+                        <style dangerouslySetInnerHTML={{__html: `
+          html, body {
+            margin: 0;
+            padding: 0;
+            text-align: center;
+          }
+          h1 {
+            font-size: 24px;
+            font-weight: bold;
+          }
+          hr {
+            margin-bottom: 40px;
+            margin-top: 40px;
+            width: 50%;
+          }
+        `}} />
+                        <InlineShareButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            color: 'social',      // set the color of buttons (social, white)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            labels: 'cta',        // button labels (cta, counts, null)
+            language: 'en',       // which language to use (see LANGUAGES)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+              'whatsapp',
+              'linkedin',
+              'messenger',
+              'facebook',
+              'twitter'
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,
+            size: 40,             // the size of each button (INTEGER)
+ 
+            // OPTIONAL PARAMETERS
+            url: 'http://localhost:3000/', // (defaults to current url)
+            image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+            description: 'custom text',       // (defaults to og:description or twitter:description)
+            title: 'custom title',            // (defaults to og:title or twitter:title)
+            message: 'custom email text',     // (only for email sharing)
+            subject: 'custom email subject',  // (only for email sharing)
+            username: 'custom twitter handle' // (only for twitter sharing)
+          }}
+        />
+        <hr/>
+       
                         <div className={styles.overallBandComponent}>
                             <article>OVERALL BAND:</article>
                             <article>{scores[0].overallBand}</article>
