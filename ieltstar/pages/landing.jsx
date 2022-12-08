e17d4ae4e9b14ee09d8fbb17c368723d12888fef
Saloni <talwar.sa@northeastern.edu>
2022-12-07 20:12:20 -0500

    Adding favicon in landing page

--- ieltstar/pages/landing.jsx
+++ ieltstar/pages/landing.jsx
@@ -5,10 +5,15 @@ import Section from "../components/LandingPage/Section";
 import AboutUs from "../components/LandingPage/AboutUs";
 import Testimonial from "../components/LandingPage/Testimonial";
 import Footer from "../components/LandingPage/Footer";
+import Head from "next/head";
 
 const Landing = () => {
   return (
     <>
+      <Head>
+        <title>IELTSTAR - Student</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+      </Head>
       <div className={styles.bgWrap}>
         <Header />
         <Hero />
