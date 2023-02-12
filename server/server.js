a3e7de73c917d3a3db707aa8b574bcae7447d1b4
Harshil Shah <shah.harshil2@northeastern.edu>
2023-02-12 03:41:12 -0500

    port fix for heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ import app from "./api/app.js";
 
 // port 8080
 const port = 8080;
-app.listen(8080, () => {
-  console.log(`server running at ${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`server running at ${process.env.PORT || port}`);
 });
